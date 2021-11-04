a0bf2bb5540a127d3002d7d1c967ca2149264be4
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-04 18:52:02 -0400

    Add test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,39 @@
 const request = require("request");
-const input = process.argv.splice(2)
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`,(error, response, body) => {
-  if(error) {
-    console.log('Oops Invalid URL!!!', error); 
-    return;
-  }
 
-  const data = JSON.parse(body);
-  // console.log(typeof data)
-  // console.log(data)
-  if(data.length === 0) {
-    console.log("Oops breed not found!");
-    return;
-  }
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error, _response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, null);
+      return;
+    }
   
-  console.log(data[0].description)
-})
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
+    callback(null,data[0].description);
+  });
+};
+
+
+
+// request(`https://api.theatapi.com/v1/breeds/search?q=${input}`,(error, response, body) => {
+//   if(error) {
+//     console.log('Oops Invalid URL!!!\n', error);
+//     return;
+//   }
+//   const data = JSON.parse(body);
+//   // console.log(typeof data)
+//   // console.log(data)
+
+//   if(data.length === 0) {
+//     console.log("Oops breed not found!");
+//     return;
+//   }
+//   console.log(data[0].description)
+// })
+
+module.exports =  { fetchBreedDescription };
